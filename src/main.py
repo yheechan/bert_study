01c6589d87c244a2e39090d5964b08e4af1e2cde
yheechan <heechy06@naver.com>
2023-01-26 23:05:35 +0900

    Overfit train

--- src/main.py
+++ src/main.py
@@ -4,9 +4,34 @@ import pprint
 import data_loader
 
 from models.bert import Bert
+import trainer
+
+import torch, gc
+import torch.nn as nn
+from torch import optim
+
+from tqdm import tqdm
+
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+
+import timeit
+import numpy as np
 
 def define_argparser():
 	p = argparse.ArgumentParser()
+
+	p.add_argument(
+		'--research_subject',
+		required=True,
+		help='The name of the research subject. (ex: server1)',
+	)
+
+	p.add_argument(
+		'--research_num',
+		required=True,
+		help='The number of current test for a subject experiment. (ex: 01)',
+	)
 
 	p.add_argument(
 		'--batch_size',
@@ -56,7 +81,7 @@ def get_crit():
 	return crit
 
 def get_optimizer(model, config):
-	optimizer = optim.Adam(model.parameters(), lr=config.lr, betas=(.9, .98))
+	optimizer = optim.Adam(model.parameters(), lr=config.lr)
 	return optimizer
 	
 
@@ -79,6 +104,106 @@ def main(config):
 	train, valid, test = data_loader.data_loader(paths,
 												batch_size=config.batch_size,
 												max_length=config.max_length)
+
+	
+	# ********** GET MODEL, LOSS FUNCTION - MOVED TO DEVICE **********
+	model = get_model(config)
+	crit = get_crit()
+
+	if torch.cuda.is_available():
+		device_num = 0
+	else:
+		device_num = -1
+	
+	print('\nUsing device number: ', device_num)
+
+	gc.collect()
+	torch.cuda.empty_cache()
+
+	if device_num >= 0:
+		model.cuda(device_num)
+		crit.cuda(device_num)
+
+
+	# ********** GET OPTIMIZER **********
+	optimizer = get_optimizer(model, config)
+
+
+	'''
+	device = next(model.parameters()).device
+
+	for epoch_i in range(10):
+	
+		# ========= TRAINING =========
+
+		#Put the model into training mode
+		model.train()
+
+		tot_train_acc = []
+		tot_train_loss = []
+
+		for batch_idx, batch in enumerate(train):
+			batch_input = batch[0]
+			batch_label = batch[1]
+
+			# *** LOAD BATCH TO DEVICE ***
+			batch_label = batch_label.to(device)
+			# |battch_label = (batch_size)
+
+			mask = batch_input['attention_mask'].to(device)
+			# |mask| = (batch_size, max_length)
+
+			input_id = batch_input['input_ids'].squeeze(1).to(device)
+			# |input_id| = (batch_size, max_length)
+
+			optimizer.zero_grad()
+
+			# *** PREDICT ***
+			y_hat = model(input_id, mask)
+			# |y_hat| = (batch_size, binary(2))
+
+			# *** CALCULATE LOSS ***
+			loss = crit(y_hat, batch_label)
+			tot_train_loss.append(loss.item())
+
+			# *** CALCULATE ACCURACY ***
+			pred = y_hat.argmax(1).flatten()
+			acc = (pred == batch_label).cpu().numpy().mean() * 100
+			tot_train_acc.append(acc)
+
+			# *** TRAIN MODEL ***
+			loss.backward()
+			optimizer.step()
+
+		# Calculate the average loss over the entire training data
+		train_loss = np.mean(tot_train_loss)
+		train_acc = np.mean(tot_train_acc)
+
+		print(epoch_i, train_loss, train_acc)
+	'''
+	# ********** INSTANTIATE TENSORBOARD **********
+	subject_title = config.research_subject
+
+	timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+	writer = SummaryWriter('../tensorboard/'+subject_title+'/tests')
+
+	title = subject_title + '_' + config.research_num
+
+	start_time = timeit.default_timer()
+
+
+	# ********** TRAIN MODEL **********
+	trainer.train(
+		model=model,
+		crit=crit,
+		optimizer=optimizer,
+		train_loader=train,
+		valid_loader=valid,
+		n_epochs=config.n_epochs,
+		writer=writer,
+		title=title
+	)
+
 
 if __name__ == '__main__':
 	config = define_argparser()
--- src/models/bert.py
+++ src/models/bert.py
@@ -2,16 +2,20 @@ from torch import nn
 from transformers import BertModel
 
 class Bert(nn.Module):
-	def __init__(self, dropout=0.5):
+	def __init__(self, dropout=0.0):
+		super(Bert, self).__init__()
+
 		self.bert = BertModel.from_pretrained('bert-base-uncased')
 		self.dropout = nn.Dropout(dropout)
-		self.fc = nn.Linear(768, 2)
-		self.relu = nn.ReLU()
+		self.fc1 = nn.Linear(768, 512)
+		self.relu1 = nn.ReLU()
+		self.fc2 = nn.Linear(512, 2)
+		self.relu2 = nn.ReLU()
 	
 	def forward(self, input_id, mask):
 		_, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
-		dp = self.dropout(pool_output)
-		fc = self.fc(dp)
-		y_hat = self.relu(fc)
+		dp = self.dropout(pooled_output)
+		fc1 = self.relu1(self.fc1(dp))
+		y_hat = self.relu2(self.fc2(fc1))
 
 		return y_hat
