7a80023306a5d55883985d441aeae8c89b904704
yheechan <heechy06@naver.com>
2023-02-02 18:04:09 +0900

    2.2 Temporary

--- src/models/bert.py
+++ src/models/bert.py
@@ -1,12 +1,9 @@
 from torch import nn
 from transformers import BertModel
 
-class Bert(nn.Module):
+class Classification(nn.Module):
 	def __init__(self, dropout=0.0):
-		super(Bert, self).__init__()
-
-		self.bert = BertModel.from_pretrained('bert-base-uncased')
-		# self.dp = nn.Dropout(dropout)
+		super(Classification, self).__init__()
 
 		self.fc1 = nn.Linear(768, 512)
 		self.relu1 = nn.ReLU()
@@ -19,15 +16,30 @@ class Bert(nn.Module):
 		self.fc3 = nn.Linear(512, 2)
 		self.relu3 = nn.ReLU()
 		self.dp3 = nn.Dropout(dropout)
+	
+	def forward(self, input_vector):
+		
+		fc1 = self.dp1(self.relu1(self.fc1(input_vector)))
+		fc2 = self.dp2(self.relu2(self.fc2(fc1)))
+		y_hat = self.dp3(self.relu3(self.fc3(fc2)))
+
+		return y_hat
+
+
+class Bert(nn.Module):
+	def __init__(self, dropout=0.0):
+		super(Bert, self).__init__()
+
+		self.bert = BertModel.from_pretrained('bert-base-uncased')
+		# self.dp = nn.Dropout(dropout)
+
+		self.classification = Classification(dropout=dropout)
 
 	
 	def forward(self, input_id, mask):
-		_, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
-		# |pooled_output| = (batch_size, 728) -> output of [CLS] token
-		# dp = self.dp(pooled_output)
+		_, cls_vector = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
+		# |cls_vector| = (batch_size, 728) -> output of [CLS] token
 
-		fc1 = self.dp1(self.relu1(self.fc1(pooled_output)))
-		fc2 = self.dp2(self.relu2(self.fc2(fc1)))
-		y_hat = self.dp3(self.relu3(self.fc3(fc2)))
+		y_hat = self.classification(cls_vector)
 
 		return y_hat
